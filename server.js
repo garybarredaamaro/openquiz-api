77a3c7c219031dccc37e97e0030f07cc463836c8
Gary Amaro <gary@dnamicro.com>
2023-04-21 10:10:25 +0800

    upload file

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const path = require('path')
+const jwt = require('jsonwebtoken')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '/uploads'))
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '_' + file.originalname)
+  }
+})
+const upload = multer({ storage })
+
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.get('/', (req, res) => {
   res.json({
     message: 'yey'
+  })
+})
+
+app.post('/user', upload.single('profile'), (req, res) => {
+  const data = {
+    ...req.body,
+    profile: req.file.filename
+  }
+  const token = jwt.sign({ data }, 'secret')
+  res.json({
+    success: true,
+    data,
+    token
   })
 })
 
